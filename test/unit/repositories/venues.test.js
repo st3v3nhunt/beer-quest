ad7e184c7ae94633aa6e0652954027c7258c3afd
Steve Hunt <st3v3nhunt@users.noreply.github.com>
2022-05-08 11:27:05 +0100

    refactor: isolate modules in test, improve organisation

--- test/unit/repositories/venues.test.js
+++ test/unit/repositories/venues.test.js
@@ -1,5 +1,3 @@
-const { getAll, init, getByLocation } = require('../../../src/repositories/venues')
-
 function createVenues (amount) {
   const venues = []
   for (let i = 0; i < amount; i++) {
@@ -7,74 +5,102 @@ function createVenues (amount) {
   }
   return venues
 }
-const loadData = require('../../../src/lib/load-data')
-jest.mock('../../../src/lib/load-data')
 
 describe('venues repository', () => {
-  const venues = createVenues(100)
+  let venuesRepo
+  let loadData
+  const venues100 = createVenues(100)
 
-  beforeAll(async () => {
-    jest.resetAllMocks()
-    loadData.mockResolvedValueOnce(venues)
-    await init()
+  beforeEach(async () => {
+    jest.resetModules()
+    venuesRepo = require('../../../src/repositories/venues')
+    loadData = require('../../../src/lib/load-data')
+    jest.mock('../../../src/lib/load-data')
   })
 
-  test('init calls loads data once', async () => {
-    await init()
-    await init()
+  async function initRepo (data) {
+    loadData.mockResolvedValueOnce(data)
+    await venuesRepo.init()
+  }
 
-    expect(loadData).toHaveBeenCalledTimes(1)
-  })
+  describe('init', () => {
+    test('loads data once when data is returned', async () => {
+      await initRepo(venues100)
+      await venuesRepo.init()
+      await venuesRepo.init()
 
-  test('getAll returns 50 by default', async () => {
-    const res = await getAll()
-
-    expect(res.length).toEqual(50)
-  })
-
-  test('getAll returns pageSize when less than 50', async () => {
-    const pageSize = 49
-
-    const res = await getAll(pageSize)
-
-    expect(res.length).toEqual(pageSize)
-  })
-
-  test('getAll returns 50 when pageSize is greater than 50', async () => {
-    const pageSize = 51
-
-    const res = await getAll(pageSize)
-
-    expect(res.length).toEqual(50)
-  })
-
-  test.each([
-    { includeDistance: false },
-    { includeDistance: true }
-  ])('getByLocation returns all venues ordered by distance from coords and does not mutate venues', async ({ includeDistance }) => {
-    const lat = '55'
-    const lng = '-1.5'
-    const coords = { lat, lng }
-
-    const res = await getByLocation(coords, includeDistance)
-
-    expect(res.length).toEqual(venues.length)
-    expect(res[0].id).toEqual(0)
-    expect(res[99].id).toEqual(99)
-
-    let currDist = 0
-    let prevDist = 0
-    res.forEach(x => {
-      if (includeDistance) {
-        expect(x).toHaveProperty('distance')
-        expect(currDist).toBeGreaterThanOrEqual(prevDist)
-        prevDist = currDist
-        currDist = x.distance
-      } else {
-        expect(x).not.toHaveProperty('distance')
-      }
+      expect(loadData).toHaveBeenCalledTimes(1)
     })
-    const venuesWithDistance = (await getAll()).filter(x => x.distance !== undefined)
-    expect(venuesWithDistance.length).toEqual(0)
+
+    test('loads data until it has loaded data', async () => {
+      await venuesRepo.init()
+      await venuesRepo.init()
+      await initRepo(venues100)
+      await venuesRepo.init()
+      await venuesRepo.init()
+
+      expect(loadData).toHaveBeenCalledTimes(3)
+    })
+  })
+
+  describe('getAll', () => {
+    test('returns 50 by default', async () => {
+      await initRepo(venues100)
+
+      const res = await venuesRepo.getAll()
+
+      expect(res.length).toEqual(50)
+    })
+
+    test('returns pageSize when less than 50', async () => {
+      await initRepo(venues100)
+      const pageSize = 49
+
+      const res = await venuesRepo.getAll(pageSize)
+
+      expect(res.length).toEqual(pageSize)
+    })
+
+    test('returns 50 when pageSize is greater than 50', async () => {
+      await initRepo(venues100)
+      const pageSize = 51
+
+      const res = await venuesRepo.getAll(pageSize)
+
+      expect(res.length).toEqual(50)
+    })
+  })
+
+  describe('getByLocation', () => {
+    test.each([
+      { includeDistance: false },
+      { includeDistance: true }
+    ])('returns all venues ordered by distance from coords and does not mutate venues', async ({ includeDistance }) => {
+      await initRepo(venues100)
+      const lat = '55'
+      const lng = '-1.5'
+      const coords = { lat, lng }
+
+      const res = await venuesRepo.getByLocation(coords, includeDistance)
+
+      expect(res.length).toEqual(venues100.length)
+      expect(res[0].id).toEqual(0)
+      expect(res[99].id).toEqual(99)
+
+      let currDist = 0
+      let prevDist = 0
+      res.forEach(x => {
+        if (includeDistance) {
+          expect(x).toHaveProperty('distance')
+          expect(currDist).toBeGreaterThanOrEqual(prevDist)
+          prevDist = currDist
+          currDist = x.distance
+        } else {
+          expect(x).not.toHaveProperty('distance')
+        }
+      })
+      const venuesWithDistance = (await venuesRepo.getAll()).filter(x => x.distance !== undefined)
+      expect(venuesWithDistance.length).toEqual(0)
+    })
   })
 })
